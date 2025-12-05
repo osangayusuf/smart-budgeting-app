eb31e1b1d544031acf4ade4d7075b967d959a05a
Osanga Yusuf <yusufjenom@gmail.com>
2025-12-05 16:10:40 +0100

    Refactor app structure by encapsulating page definitions and navigation logic within a main function. Added a conditional to run the main function when the script is executed directly.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
+def main():
 # Define the pages
-home = st.Page("home.py", title="Home", icon="🏠", default=True)
-add_expenses = st.Page("add_expenses.py", title="Add Expenses", icon="💸")
-view_expenses = st.Page("view_expenses.py", title="View Expenses", icon="📊")
-insights = st.Page("insights.py", title="Insights", icon="📈")
-# Set up navigation
-pg = st.navigation([home, add_expenses, view_expenses, insights])
+    home = st.Page("home.py", title="Home", icon="🏠", default=True)
+    add_expenses = st.Page("add_expenses.py", title="Add Expenses", icon="💸")
+    view_expenses = st.Page("view_expenses.py", title="View Expenses", icon="📊")
+    insights = st.Page("insights.py", title="Insights", icon="📈")
 
-# Run the selected page
-pg.run()
+    # Set up navigation
+    pg = st.navigation([home, add_expenses, view_expenses, insights])
+
+    # Run the selected page
+    pg.run()
+
+if __name__ == "__main__":
+    main()
